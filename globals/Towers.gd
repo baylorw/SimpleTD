281f9139f207b3d7fc599dea07dbed1628ba7d0f
baylor wetzel <baylorw@yahoo.com>
2024-08-15 00:56:19 -0500

    Initial commit

--- globals/Towers.gd
+++ globals/Towers.gd
@@ -17,23 +17,23 @@ func load_tower(name: String):
 	var new_tower = tower_data.instantiate()
 	return new_tower
 	
-var tower_data = {
-	"blue1": {
-		"damage": 5,
-		"rof": 3,
-		"range": 350,
-		"category": "bullet"
-	},
-	"green1": {
-		"damage": 20,
-		"rof": 1,
-		"range": 350,
-		"category": "bullet"
-	},
-	"red1": {
-		"damage": 100,
-		"rof": 0.3,
-		"range": 550,
-		"category": "rocket"
-	}
-}
+#var tower_data = {
+	#"blue1": {
+		#"damage": 5,
+		#"rof": 3,
+		#"range": 350,
+		#"category": "bullet"
+	#},
+	#"green1": {
+		#"damage": 20,
+		#"rof": 1,
+		#"range": 350,
+		#"category": "bullet"
+	#},
+	#"red1": {
+		#"damage": 100,
+		#"rof": 0.3,
+		#"range": 550,
+		#"category": "rocket"
+	#}
+#}
